1d522682e7660e3faf4f5070a919f1d1be0534a7
hahmadqayyum <hahmadqayyum@gmail.com>
2020-07-03 15:38:41 +0500

    remove error

--- src/components/nav-components/Analysis.js
+++ src/components/nav-components/Analysis.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import './Analysis.css'
 // https://corona.lmao.ninja/v2/countries
 function Analysis() {
     const [country, setCountry] = useState([{}]);
