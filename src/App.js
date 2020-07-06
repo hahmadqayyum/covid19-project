de8dd66f0a9d2e273b33ba0fa1ed3d67839abe70
hahmadqayyum <hahmadqayyum@gmail.com>
2020-07-07 00:27:58 +0500

    bottom nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 
 import Nav from "./components/nav-components/Nav";
+import BottomNav from './components/nav-components/BottomNav';
 //https://covid19.mathdro.id/api
 // https:api.covid19api.com/summary
 // https://corona.lmao.ninja/v2/all
@@ -17,7 +18,7 @@ function App() {
     <div className="App">
       <Nav />
       
-      
+      <BottomNav />
 
 
     </div>
---
+++ src/components/nav-components/BottomNav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './BottomNav.css'
+
+
+function BottomNav() {
+    return (
+        <nav className="nav">
+            <ul className="links">
+                <li>Ahmad Qayyum</li>
+                <li>Email: hahmadqayyum@gmail.com</li>
+            </ul>
+        </nav>
+    )
+}
+export default BottomNav;
